f2ab0de420d5fbe5d5594bf313b7a0a0146b2273
Gonzalo Castiella <gonzalo.castiella@gmail.com>
2019-01-31 20:08:39 +0100

    Added small game manager only for leaving Level1 scene. Fixed error of not destroying all loading screen when reaching the loaded scene.

---
+++ TimeCrisis/Assets/Scripts/Managers/GameMan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMan : MonoBehaviour
+{
+    //public static GameMan Instance { get; private set; }
+
+    //private void Awake()
+    //{
+    //    if (Instance != null)
+    //        Instance = this;
+    //    else
+    //        Destroy(gameObject);
+    //}
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            SceneMan.Instance.LoadSceneByEnum("MainMenu");
+        }
+    }
+}

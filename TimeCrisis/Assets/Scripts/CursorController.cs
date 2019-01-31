08158cc6ee2a6b15a65ca43859bae5a8766726ab
Gonzalo Castiella <gonzalo.castiella@gmail.com>
2019-01-31 19:45:41 +0100

    Added Unity Standard Assets to use FirstPersonController. Only added necessary scripts to avoid taking whole Standard Assets.
    Added a cursor to have visual feedback of where to point at.
    Implemented first animations to crouch and stand.

---
+++ TimeCrisis/Assets/Scripts/CursorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    Canvas my_canvas;
+
+    // Use this for initialization
+    void Start()
+    {
+        my_canvas = gameObject.GetComponentInParent<Canvas>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCursor();
+    }
+
+    void MoveCursor()
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(my_canvas.transform as RectTransform,
+                Input.mousePosition, my_canvas.worldCamera, out pos);
+        transform.position = my_canvas.transform.TransformPoint(pos);
+    }
+
+
+}
---
+++ TimeCrisis/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Transform player_cam;
+    Animator anim_p;
+
+    private void Start()
+    {
+        player_cam = transform.Find("FirstPersonCharacter");
+        anim_p = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Crouch();
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Stand();
+        }
+    }
+
+    void Crouch()
+    {
+        anim_p.SetBool("IsCrouch", true);
+    }
+
+    void Stand()
+    {
+        anim_p.SetBool("IsCrouch", false);
+    }
+}
